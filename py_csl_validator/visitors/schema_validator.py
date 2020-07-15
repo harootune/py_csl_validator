8b87eb0d296b008a371f342c4f00049280228062
Otis, Derek Harootune <dhotis2@illinois.edu>
2020-07-15 09:31:01 -0500

    Switch to Explicit Context Definition using Recursive Stacks
    Summary:
    Use of a single shared stack for the interpreter was clever, but ultimately required too much wrenching to accommodate expressions which defined their own sub-contexts (such as parentheticals and some string provider children which take string providers as their own children). To avoid a hardcoded check for such expressions, which would be unacceptably fragile, I've switched to a model in which sub-stacks are recursively generated for each expression by calling .visit on each child.
    
    Additions:
    + Recursive stack generation
    > Each ._call_expression target takes a stack as an argument
    > The stack is generated within .visit by recursively calling .visit on each child of the current node
    > This delinaeates each expression's context while maintaining a single unified visit behavior, instead of defining that per-expression
    + Adjusted lark representation of CSL 1.2 grammar to allow for the extraction of certain values, like version numbers
    
    TODO
    * Interpretation behavior for
    > Combinatorial Expressions
    > Date expressions
    > Conditional expressions
    > Column directives
    > Global directives
    * Validation behavior
    * Test suite
    * GUI?

--- py_csl_validator/visitors/schema_validator.py
+++ py_csl_validator/visitors/schema_validator.py
@@ -5,9 +5,4 @@ from .csl_visitor import CslVisitor
 class CslRuleset(CslVisitor):
 
     def __init__(self, tree):
-        super().__init__()
-
-        self.visit(tree)
-
-        self.schema = self.stack.pop()
-
+        super().__init__(tree)
